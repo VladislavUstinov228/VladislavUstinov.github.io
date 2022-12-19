dedee20baae5d5e848201a94b39e7bbe70a7e7dd
Влад Устинов <98713367+vladislavustinov1@users.noreply.github.com>
2022-12-19 04:25:41 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -31,9 +31,9 @@ let item = 0;
 
 let fun = setInterval(function () {
   item += 1;
-  if (item === 11) {
+  if (item === 10) {
     document.getElementById("au").play();
-    item -= 10;
+    item -= 9;
   }
   txt.innerHTML = item;
 }, 1000);
